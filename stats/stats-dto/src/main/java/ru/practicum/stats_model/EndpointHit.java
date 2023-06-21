814d3dd76ab2e29737e82ecd9e61b99ba06673b6
Dmitry Lebedev <leibnitz@mail.ru>
2023-06-22 00:25:04 +0300

    EWM Part 1 - Statistics HotFix#1

--- stats/stats-dto/src/main/java/ru/practicum/stats_model/EndpointHit.java
+++ stats/stats-dto/src/main/java/ru/practicum/stats_model/EndpointHit.java
@@ -1,6 +1,7 @@
 package ru.practicum.stats_model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
@@ -8,6 +9,8 @@ import java.time.LocalDateTime;
 @Builder
 @Data
 public class EndpointHit {
+    private static final String DATE_TIME_FORMATTER = "yyyy-MM-dd HH:mm:ss";
+
     @NotBlank
     private String app;
 
@@ -18,5 +21,6 @@ public class EndpointHit {
     private String ip;
 
     @NotBlank
+    @DateTimeFormat(pattern = DATE_TIME_FORMATTER)
     private LocalDateTime timestamp;
 }
